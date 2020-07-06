7f5de8d47e4e6ffe7dcc17055ef0c605ad197d08
Stunnerr <40601270+Stunnerr@users.noreply.github.com>
2020-07-07 02:49:36 +0500

    Update Reposter.py

--- Reposter.py
+++ Reposter.py
@@ -65,7 +65,7 @@ class RepostMod(loader.Module):
 		for peer in peers:
 			if mymsg: 
 				api.messages.send(v=5.125,peer_id=peer, random_id=random.randint(1, 999999999),message=mymsg)
-			time.sleep(200)
+			time.sleep(0.2)
 			api.messages.send(v=5.125,peer_id=peer, random_id=random.randint(1, 999999999),message=post,attachment=upload)
-			time.sleep(200)
+			time.sleep(0.2)
 		await message.edit("`Готово`")
