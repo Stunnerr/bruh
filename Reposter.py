8d6cf0c268ccb72a80b869f44e253085b3ca6330
Stunnerr <40601270+Stunnerr@users.noreply.github.com>
2020-07-06 16:14:30 +0500

    Изменил вид отправляемых сообщений т

--- Reposter.py
+++ Reposter.py
@@ -25,16 +25,20 @@ class RepostMod(loader.Module):
 		if 0 in peers:
 			await utils.answer(message, "Вы не указали или указали неверно, кому хотите писать в конфиге")
 			return
-		fname = str(random.randint(1,100)) + '.png'
-		mymsg = "\nКомментарий:" + '\u2002'.join(('\n'+ args).splitlines(True)) if args else ""
-		post = '\nТекст поста/пересылаемого сообщения:'+ '\u2002'.join(('\n' + reply.message).splitlines(True)) if reply.message else ""
+		await message.edit("'Подготовка...'")
+		cid = reply.fwd_from.channel_id
+		channel = await message.client.get_entity(cid)
+		mymsg = args
+		post = '\nОтправлено из'+ channel.title+':'+'\u2002'.join(('\n' + reply.message).splitlines(True)) if reply.message else ""
 		msg = mymsg + post 
 		token = self.config["API_TOKEN"]
 		session = vk.Session(access_token=token)
 		api = vk.API(session)
 		doc = reply.photo
 		upload = ""
+		await message.edit("'Поиск вложений...'")
 		if doc:
+			await message.edit("'Загрузка фото...'")
 			path = await reply.download_media()
 			url = api.photos.getMessagesUploadServer(v=5.125)['upload_url']
 			files = {'file':(path, open(path, 'rb'))}
@@ -44,16 +48,18 @@ class RepostMod(loader.Module):
 			upload = f"photo{save[0]['owner_id']}_{save[0]['id']}"
 		doc = reply.video
 		if doc:
+			await message.edit("'Загрузка видео...'")
 			path = await reply.download_media()
-			cid = reply.fwd_from.channel_id
 			title = None
 			if cid:
-				channel = await message.client.get_entity(cid)
 				title = 'Video from ' + channel.title
 			data = api.video.save(v=5.125, title=title, is_private=1)
 			files = {'file':(path, open(path, 'rb'))}
 			r = requests.post(data['upload_url'], files=files)
 			upload += f",video{data['owner_id']}_{data['video_id']}"
+		await message.edit("'Отправка...'")
 		for peer in peers:
-			api.messages.send(v=5.125,peer_id=peer, random_id=random.randint(1, 999999999),message=msg,attachment=upload)
+			if mymsg: 
+				api.messages.send(v=5.125,peer_id=peer, random_id=random.randint(1, 999999999),message=mymsg)
+			api.messages.send(v=5.125,peer_id=peer, random_id=random.randint(1, 999999999),message=post,attachment=upload)
 		await message.edit("Success")
