d6e38a3b558a3f2dd2a4b8c52fcb2968ef56e1d6
Stunnerr <40601270+Stunnerr@users.noreply.github.com>
2020-08-16 02:10:49 +0500

    a

--- autovoter.py
+++ autovoter.py
@@ -1,18 +1,14 @@
+from .. import loader, utils
 from telethon import events
 from telethon.tl.custom import Message
 from telethon.tl.functions.messages import SendVoteRequest
 
-from userbot import Client
 
-handlers = []
-
-
-def register(client: Client):
+class autovotermod(loader.Module):
     data = {
         'bot_id': 931448009,
         'chat_id': -1001227323951,
-        'option': b"4",
-        'tag': "@TheEvilBird @KDZHR @didedoshka @stunnerer"
+        'option': b"4"
     }
 
     @client.on(events.NewMessage(from_users=[data['bot_id']], chats=[data["chat_id"]]))
