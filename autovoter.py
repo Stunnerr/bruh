4db4acd866076d4ffe1b14962903516584b4645c
Stunnerr <40601270+Stunnerr@users.noreply.github.com>
2020-08-16 11:14:19 +0500

    test

--- autovoter.py
+++ autovoter.py
@@ -11,7 +11,7 @@ class autovotermod(loader.Module):
         'option': b"4"
     }
 
-    @client.on(events.NewMessage(from_users=[data['bot_id']], chats=[data["chat_id"]]))
+    @borg.on(events.NewMessage(from_users=[data['bot_id']], chats=[data["chat_id"]]))
     async def handler(message: Message):
         if not message.poll:
             return
