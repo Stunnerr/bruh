36dfe61d1b846a3dda67d6cebadb475cb6f72d77
Stunnerr <40601270+Stunnerr@users.noreply.github.com>
2020-08-16 20:33:54 +0500

    Update autovoter.py

--- autovoter.py
+++ autovoter.py
@@ -23,6 +23,9 @@ class autovoterMod(loader.Module):
         client = message.client
         await client.send_message(await client.get_me(), 'handler')
         await client(SendVoteRequest(data["chat_id"], message.id, [data["option"]]))
+    async def aaaacmd(self, message):
+        await message.respond(self._client)
     async def client_ready(self, client, db):
+        self._client = client
         client.add_event_handler(handler)
         await client.send_message(await client.get_me(), 'client_ready')
